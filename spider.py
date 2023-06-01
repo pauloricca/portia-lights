105c32a82c1f6ac9aa30651e72f472598cd9101c
Paulo Ricca <pauloricca@gmail.com>
2023-06-01 14:36:56 +0100

    auto detect master

--- spider.py
+++ spider.py
@@ -4,7 +4,19 @@
 # adafruit-circuitpython-pixelbuf https://github.com/adafruit/Adafruit_CircuitPython_Pixelbuf
 
 from app import App
+from constants import *
+import subprocess
 
 # Check for audio interface present. if so, this is master
+isMaster = False
+if PLATFORM == 'Linux':
+    audioCardsOutput = subprocess.check_output(("cat", "/proc/asound/cards")).decode("ascii")
+    audioCardsOutputLines = audioCardsOutput.split('\n')
+    for line in audioCardsOutputLines:
+        if "USB-Audio" in line:
+            isMaster = True
 
-app = App(isMaster=False, isLightController=True, isVerbose=False)
+if isMaster: print("Starting as Master")
+else: print("Starting as Slave")
+
+app = App(isMaster=isMaster, isLightController=True, isVerbose=False)
